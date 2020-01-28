a991a26b9ace843965251959e7a866a01d0ae3b5
Ethan Rennick <45255649+EthanRennick@users.noreply.github.com>
2020-01-28 14:09:16 +0000

    Add files via upload

--- opengl-3-electric-bigalee/master/SFMLOpenGL/Game.cpp
+++ opengl-3-electric-bigalee/master/SFMLOpenGL/Game.cpp
@@ -45,32 +45,13 @@ GLubyte triangles[36]; //triangle points
 GLuint vbo[1];
 GLuint index;
 
-vector<gpp::Vector3> cube;
-
 void Game::initialize()
 {
 	isRunning = true;
-	cube.push_back(gpp::Vector3(1.0f, -1.0f, -5.0f)); //3 - 0
-	cube.push_back(gpp::Vector3(-1.0f, -1.0f, -5.0f)); //2 - 1
-	cube.push_back(gpp::Vector3(-1.0f, 1.0f, -5.0f)); //1 - 2
-	cube.push_back(gpp::Vector3(1.0f, 1.0f, -5.0f)); //4 - 3
-	cube.push_back(gpp::Vector3(1.0f, -1.0f, -10.0f)); //7 - 4
-	cube.push_back(gpp::Vector3(1.0f, 1.0f, -10.0f)); //8 - 5
-	cube.push_back(gpp::Vector3(-1.0f, -1.0f, -10.0f)); //6 - 6
-	cube.push_back(gpp::Vector3(-1.0f, 1.0f, -10.0f)); //5 - 7
+
 	glewInit();
 
-	// Vertices for one Triangle
-	float vertices[] = { 1.0f, 1.0f, -5.0f, //1
-	-1.0f, 1.0f, -5.0f, //2
-	-1.0f, -1.0f, -5.0f,  //3
-	1.0f, -1.0f, -5.0f, //4
-	-1.0f, 1.0f, -10.0f, //5
-	-1.0f, -1.0f, -10.0f, //6
-	1.0f, -1.0f, -10.0f, //7
-	1.0f, 1.0f, -10.0f //8
-	};
-	
+
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glTranslatef(0, 0, -8);
 	glMatrixMode(GL_PROJECTION); //small, far away
@@ -79,87 +60,40 @@ void Game::initialize()
 	glMatrixMode(GL_MODELVIEW); //object moves, not us, not our view of it. its mass doesnt change
 
 	/* Vertices counter-clockwise winding */
-	triangles[0] = (cube.at(0).x); 
-	triangles[1] = (cube.at(0).y); 
-	triangles[2] = (cube.at(0).z); 
-
-	triangles[3] = (cube.at(1).x);
-	triangles[4] = (cube.at(1).y);
-	triangles[5] = (cube.at(1).z);
-
-	triangles[6] = (cube.at(2).x);
-	triangles[7] = (cube.at(2).y);
-	triangles[8] = (cube.at(2).z);
-
-	triangles[9] = (cube.at(3).x);
-	triangles[10] = (cube.at(3).y);
-	triangles[11] = (cube.at(3).z);
-
-	triangles[12] = (cube.at(2).x);
-	triangles[12] = (cube.at(2).y);
-	triangles[14] = (cube.at(2).z);
-
-	triangles[15] = (cube.at(0).x);
-	triangles[16] = (cube.at(0).y);
-	triangles[17] = (cube.at(0).z);
 
 
-	triangles[18] = (cube.at(4).x);
-	triangles[19] = (cube.at(4).y);
-	triangles[20] = (cube.at(4).z);
+	vertex[0].coordinate[0] = -1.0; // 2
+	vertex[0].coordinate[1] = -1.0;
+	vertex[0].coordinate[2] = 1.0;
 
-	triangles[21] = (cube.at(5).x);
-	triangles[22] = (cube.at(5).y);
-	triangles[23] = (cube.at(5).z);
+	vertex[1].coordinate[0] = 1.0; //3
+	vertex[1].coordinate[1] = -1.0;
+	vertex[1].coordinate[2] = 1.0;
 
-	triangles[24] = (cube.at(6).x);
-	triangles[25] = (cube.at(6).y);
-	triangles[26] = (cube.at(6).z);
+	vertex[2].coordinate[0] = 1.0; //4
+	vertex[2].coordinate[1] = 1.0;
+	vertex[2].coordinate[2] = 1.0;
 
-	triangles[27] = (cube.at(7).x);
-	triangles[28] = (cube.at(7).y);
-	triangles[29] = (cube.at(7).z);
+	vertex[3].coordinate[0] = -1.0; //1
+	vertex[3].coordinate[1] = 1.0;  
+	vertex[3].coordinate[2] = 1.0;
 
-	triangles[30] = (cube.at(0).x);
-	triangles[31] = (cube.at(0).y);
-	triangles[32] = (cube.at(0).z);
+	vertex[4].coordinate[0] = -1.0; 
+	vertex[4].coordinate[1] = -1.0;  
+	vertex[4].coordinate[2] = -1.0;
 
-	triangles[33] = (cube.at(5).x);
-	triangles[34] = (cube.at(5).y);
-	triangles[35] = (cube.at(5).z);
+	vertex[5].coordinate[0] = 1.0; 
+	vertex[5].coordinate[1] = -1.0;  
+	vertex[5].coordinate[2] = -1.0;
 
+	//face 3
+	vertex[6].coordinate[0] = 1.0;
+	vertex[6].coordinate[1] = 1.0;
+	vertex[6].coordinate[2] = -1.0;
 
-	vertex[0].coordinate[0] = (cube.at(0).x); //3
-	vertex[0].coordinate[1] = (cube.at(0).y); //3
-	vertex[0].coordinate[2] = (cube.at(0).z); //3
-
-	vertex[1].coordinate[0] = (cube.at(1).x); //1
-	vertex[1].coordinate[1] = (cube.at(1).y); //1
-	vertex[1].coordinate[2] = (cube.at(1).z); //1
-
-	vertex[2].coordinate[0] = (cube.at(2).x); //2
-	vertex[2].coordinate[1] = (cube.at(2).y); //2
-	vertex[2].coordinate[2] = (cube.at(2).z); //2
-
-	vertex[3].coordinate[0] = (cube.at(3).x); //3
-	vertex[3].coordinate[1] = (cube.at(3).y); //3
-	vertex[3].coordinate[2] = (cube.at(3).z); //3
-
-	vertex[4].coordinate[0] = (cube.at(4).x); //1
-	vertex[4].coordinate[1] = (cube.at(4).y); //1
-	vertex[4].coordinate[2] = (cube.at(4).z); //1
-
-	vertex[5].coordinate[0] = (cube.at(5).x); //2
-	vertex[5].coordinate[1] = (cube.at(5).y); //2
-	vertex[5].coordinate[2] = (cube.at(5).z); //2
-
-	vertex[6].coordinate[0] = (cube.at(6).x); //1
-	vertex[6].coordinate[1] = (cube.at(6).y); //1
-	vertex[6].coordinate[2] = (cube.at(6).z); //1
-
-	vertex[7].coordinate[0] = (cube.at(7).x); //2
-	vertex[7].coordinate[1] = (cube.at(7).y); //2
-	vertex[7].coordinate[2] = (cube.at(7).z); //2
+	vertex[7].coordinate[0] = -1.0;
+	vertex[7].coordinate[1] = 1.0;
+	vertex[7].coordinate[2] = -1.0;
 
 	vertex[0].color[0] = 0.1f;
 	vertex[0].color[1] = 1.0f;
@@ -220,336 +154,24 @@ void Game::update()
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 	{
-		for (int i = 0; i < 8; i++)
+		for (int i = 0; i < 6; i++)
 		{
-			cube[i] = Matrix3::RotationY(-.1) * cube[i];
-
+			vertex[i].coordinate[0] += 0.0001f;
+			vertex[i].coordinate[1] += 0.0001f;
+			vertex[i].coordinate[2] += 0.0001f;
 		}
-		vertex[0].coordinate[0] = (cube.at(0).x); //3
-		vertex[0].coordinate[1] = (cube.at(0).y); //3
-		vertex[0].coordinate[2] = (cube.at(0).z); //3
-
-		vertex[1].coordinate[0] = (cube.at(1).x); //1
-		vertex[1].coordinate[1] = (cube.at(1).y); //1
-		vertex[1].coordinate[2] = (cube.at(1).z); //1
-
-		vertex[2].coordinate[0] = (cube.at(2).x); //2
-		vertex[2].coordinate[1] = (cube.at(2).y); //2
-		vertex[2].coordinate[2] = (cube.at(2).z); //2
-
-		vertex[3].coordinate[0] = (cube.at(3).x); //3
-		vertex[3].coordinate[1] = (cube.at(3).y); //3
-		vertex[3].coordinate[2] = (cube.at(3).z); //3
-
-		vertex[4].coordinate[0] = (cube.at(4).x); //1
-		vertex[4].coordinate[1] = (cube.at(4).y); //1
-		vertex[4].coordinate[2] = (cube.at(4).z); //1
-
-		vertex[5].coordinate[0] = (cube.at(5).x); //2
-		vertex[5].coordinate[1] = (cube.at(5).y); //2
-		vertex[5].coordinate[2] = (cube.at(5).z); //2
-
-		vertex[6].coordinate[0] = (cube.at(6).x); //1
-		vertex[6].coordinate[1] = (cube.at(6).y); //1
-		vertex[6].coordinate[2] = (cube.at(6).z); //1
-
-		vertex[7].coordinate[0] = (cube.at(7).x); //2
-		vertex[7].coordinate[1] = (cube.at(7).y); //2
-		vertex[7].coordinate[2] = (cube.at(7).z); //2
 	}
 
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
 	{
-		for (int i = 0; i < 8; i++)
+		for (int i = 0; i < 6; i++)
 		{
-			cube[i] = Matrix3::RotationY(.1) * cube[i];
-
+			vertex[i].coordinate[0] -= 0.0001f;
+			vertex[i].coordinate[1] -= 0.0001f;
+			vertex[i].coordinate[2] -= 0.0001f;
 		}
-
-		vertex[0].coordinate[0] = (cube.at(0).x); //3
-		vertex[0].coordinate[1] = (cube.at(0).y); //3
-		vertex[0].coordinate[2] = (cube.at(0).z); //3
-
-		vertex[1].coordinate[0] = (cube.at(1).x); //1
-		vertex[1].coordinate[1] = (cube.at(1).y); //1
-		vertex[1].coordinate[2] = (cube.at(1).z); //1
-
-		vertex[2].coordinate[0] = (cube.at(2).x); //2
-		vertex[2].coordinate[1] = (cube.at(2).y); //2
-		vertex[2].coordinate[2] = (cube.at(2).z); //2
-
-		vertex[3].coordinate[0] = (cube.at(3).x); //3
-		vertex[3].coordinate[1] = (cube.at(3).y); //3
-		vertex[3].coordinate[2] = (cube.at(3).z); //3
-
-		vertex[4].coordinate[0] = (cube.at(4).x); //1
-		vertex[4].coordinate[1] = (cube.at(4).y); //1
-		vertex[4].coordinate[2] = (cube.at(4).z); //1
-
-		vertex[5].coordinate[0] = (cube.at(5).x); //2
-		vertex[5].coordinate[1] = (cube.at(5).y); //2
-		vertex[5].coordinate[2] = (cube.at(5).z); //2
-
-		vertex[6].coordinate[0] = (cube.at(6).x); //1
-		vertex[6].coordinate[1] = (cube.at(6).y); //1
-		vertex[6].coordinate[2] = (cube.at(6).z); //1
-
-		vertex[7].coordinate[0] = (cube.at(7).x); //2
-		vertex[7].coordinate[1] = (cube.at(7).y); //2
-		vertex[7].coordinate[2] = (cube.at(7).z); //2
 	}
-
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-	{
-		for (int i = 0; i < 8; i++)
-		{
-			cube[i] = Matrix3::RotationX(-.1) * cube[i];
-
-		}
-
-		vertex[0].coordinate[0] = (cube.at(0).x); //3
-		vertex[0].coordinate[1] = (cube.at(0).y); //3
-		vertex[0].coordinate[2] = (cube.at(0).z); //3
-
-		vertex[1].coordinate[0] = (cube.at(1).x); //1
-		vertex[1].coordinate[1] = (cube.at(1).y); //1
-		vertex[1].coordinate[2] = (cube.at(1).z); //1
-
-		vertex[2].coordinate[0] = (cube.at(2).x); //2
-		vertex[2].coordinate[1] = (cube.at(2).y); //2
-		vertex[2].coordinate[2] = (cube.at(2).z); //2
-
-		vertex[3].coordinate[0] = (cube.at(3).x); //3
-		vertex[3].coordinate[1] = (cube.at(3).y); //3
-		vertex[3].coordinate[2] = (cube.at(3).z); //3
-
-		vertex[4].coordinate[0] = (cube.at(4).x); //1
-		vertex[4].coordinate[1] = (cube.at(4).y); //1
-		vertex[4].coordinate[2] = (cube.at(4).z); //1
-
-		vertex[5].coordinate[0] = (cube.at(5).x); //2
-		vertex[5].coordinate[1] = (cube.at(5).y); //2
-		vertex[5].coordinate[2] = (cube.at(5).z); //2
-
-		vertex[6].coordinate[0] = (cube.at(6).x); //1
-		vertex[6].coordinate[1] = (cube.at(6).y); //1
-		vertex[6].coordinate[2] = (cube.at(6).z); //1
-
-		vertex[7].coordinate[0] = (cube.at(7).x); //2
-		vertex[7].coordinate[1] = (cube.at(7).y); //2
-		vertex[7].coordinate[2] = (cube.at(7).z); //2
-
-	}
-
-
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-	{
-		for (int i = 0; i < 8; i++)
-		{
-			cube[i] = Matrix3::RotationX(.1) * cube[i];
-
-		}
-		vertex[0].coordinate[0] = (cube.at(0).x); //3
-		vertex[0].coordinate[1] = (cube.at(0).y); //3
-		vertex[0].coordinate[2] = (cube.at(0).z); //3
-
-		vertex[1].coordinate[0] = (cube.at(1).x); //1
-		vertex[1].coordinate[1] = (cube.at(1).y); //1
-		vertex[1].coordinate[2] = (cube.at(1).z); //1
-
-		vertex[2].coordinate[0] = (cube.at(2).x); //2
-		vertex[2].coordinate[1] = (cube.at(2).y); //2
-		vertex[2].coordinate[2] = (cube.at(2).z); //2
-
-		vertex[3].coordinate[0] = (cube.at(3).x); //3
-		vertex[3].coordinate[1] = (cube.at(3).y); //3
-		vertex[3].coordinate[2] = (cube.at(3).z); //3
-
-		vertex[4].coordinate[0] = (cube.at(4).x); //1
-		vertex[4].coordinate[1] = (cube.at(4).y); //1
-		vertex[4].coordinate[2] = (cube.at(4).z); //1
-
-		vertex[5].coordinate[0] = (cube.at(5).x); //2
-		vertex[5].coordinate[1] = (cube.at(5).y); //2
-		vertex[5].coordinate[2] = (cube.at(5).z); //2
-
-		vertex[6].coordinate[0] = (cube.at(6).x); //1
-		vertex[6].coordinate[1] = (cube.at(6).y); //1
-		vertex[6].coordinate[2] = (cube.at(6).z); //1
-
-		vertex[7].coordinate[0] = (cube.at(7).x); //2
-		vertex[7].coordinate[1] = (cube.at(7).y); //2
-		vertex[7].coordinate[2] = (cube.at(7).z); //2
-	}
-
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-	{
-		std::cout << "Translate attempt forward" << std::endl;
-
-		for (int i = 0; i < 8; i++)
-		{
-			cube[i] = Matrix3::Translate(-.01, 0) * cube[i];
-		}
-		vertex[0].coordinate[0] = (cube.at(0).x); //3
-		vertex[0].coordinate[1] = (cube.at(0).y); //3
-		vertex[0].coordinate[2] = (cube.at(0).z); //3
-
-		vertex[1].coordinate[0] = (cube.at(1).x); //1
-		vertex[1].coordinate[1] = (cube.at(1).y); //1
-		vertex[1].coordinate[2] = (cube.at(1).z); //1
-
-		vertex[2].coordinate[0] = (cube.at(2).x); //2
-		vertex[2].coordinate[1] = (cube.at(2).y); //2
-		vertex[2].coordinate[2] = (cube.at(2).z); //2
-
-		vertex[3].coordinate[0] = (cube.at(3).x); //3
-		vertex[3].coordinate[1] = (cube.at(3).y); //3
-		vertex[3].coordinate[2] = (cube.at(3).z); //3
-
-		vertex[4].coordinate[0] = (cube.at(4).x); //1
-		vertex[4].coordinate[1] = (cube.at(4).y); //1
-		vertex[4].coordinate[2] = (cube.at(4).z); //1
-
-		vertex[5].coordinate[0] = (cube.at(5).x); //2
-		vertex[5].coordinate[1] = (cube.at(5).y); //2
-		vertex[5].coordinate[2] = (cube.at(5).z); //2
-
-		vertex[6].coordinate[0] = (cube.at(6).x); //1
-		vertex[6].coordinate[1] = (cube.at(6).y); //1
-		vertex[6].coordinate[2] = (cube.at(6).z); //1
-
-		vertex[7].coordinate[0] = (cube.at(7).x); //2
-		vertex[7].coordinate[1] = (cube.at(7).y); //2
-		vertex[7].coordinate[2] = (cube.at(7).z); //2
-	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-	{
-		std::cout << "Translate attempt back" << std::endl;
-
-		for (int i = 0; i < 8; i++)
-		{
-			cube[i] = Matrix3::Translate(.01, 0) * cube[i];
-
-		}
-		vertex[0].coordinate[0] = (cube.at(0).x); //3
-		vertex[0].coordinate[1] = (cube.at(0).y); //3
-		vertex[0].coordinate[2] = (cube.at(0).z); //3
-
-		vertex[1].coordinate[0] = (cube.at(1).x); //1
-		vertex[1].coordinate[1] = (cube.at(1).y); //1
-		vertex[1].coordinate[2] = (cube.at(1).z); //1
-
-		vertex[2].coordinate[0] = (cube.at(2).x); //2
-		vertex[2].coordinate[1] = (cube.at(2).y); //2
-		vertex[2].coordinate[2] = (cube.at(2).z); //2
-
-		vertex[3].coordinate[0] = (cube.at(3).x); //3
-		vertex[3].coordinate[1] = (cube.at(3).y); //3
-		vertex[3].coordinate[2] = (cube.at(3).z); //3
-
-		vertex[4].coordinate[0] = (cube.at(4).x); //1
-		vertex[4].coordinate[1] = (cube.at(4).y); //1
-		vertex[4].coordinate[2] = (cube.at(4).z); //1
-
-		vertex[5].coordinate[0] = (cube.at(5).x); //2
-		vertex[5].coordinate[1] = (cube.at(5).y); //2
-		vertex[5].coordinate[2] = (cube.at(5).z); //2
-
-		vertex[6].coordinate[0] = (cube.at(6).x); //1
-		vertex[6].coordinate[1] = (cube.at(6).y); //1
-		vertex[6].coordinate[2] = (cube.at(6).z); //1
-
-		vertex[7].coordinate[0] = (cube.at(7).x); //2
-		vertex[7].coordinate[1] = (cube.at(7).y); //2
-		vertex[7].coordinate[2] = (cube.at(7).z); //2
-	}
-
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-	{
-		std::cout << "Scale attempt smol" << std::endl;
-
-		for (int i = 0; i < 8; i++)
-		{
-			cube[i] = Matrix3::Scale(99, 99) * cube[i];
-
-		}
-		vertex[0].coordinate[0] = (cube.at(0).x); //3
-		vertex[0].coordinate[1] = (cube.at(0).y); //3
-		vertex[0].coordinate[2] = (cube.at(0).z); //3
-
-		vertex[1].coordinate[0] = (cube.at(1).x); //1
-		vertex[1].coordinate[1] = (cube.at(1).y); //1
-		vertex[1].coordinate[2] = (cube.at(1).z); //1
-
-		vertex[2].coordinate[0] = (cube.at(2).x); //2
-		vertex[2].coordinate[1] = (cube.at(2).y); //2
-		vertex[2].coordinate[2] = (cube.at(2).z); //2
-
-		vertex[3].coordinate[0] = (cube.at(3).x); //3
-		vertex[3].coordinate[1] = (cube.at(3).y); //3
-		vertex[3].coordinate[2] = (cube.at(3).z); //3
-
-		vertex[4].coordinate[0] = (cube.at(4).x); //1
-		vertex[4].coordinate[1] = (cube.at(4).y); //1
-		vertex[4].coordinate[2] = (cube.at(4).z); //1
-
-		vertex[5].coordinate[0] = (cube.at(5).x); //2
-		vertex[5].coordinate[1] = (cube.at(5).y); //2
-		vertex[5].coordinate[2] = (cube.at(5).z); //2
-
-		vertex[6].coordinate[0] = (cube.at(6).x); //1
-		vertex[6].coordinate[1] = (cube.at(6).y); //1
-		vertex[6].coordinate[2] = (cube.at(6).z); //1
-
-		vertex[7].coordinate[0] = (cube.at(7).x); //2
-		vertex[7].coordinate[1] = (cube.at(7).y); //2
-		vertex[7].coordinate[2] = (cube.at(7).z); //2
-	}
-
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-	{
-		std::cout << "Scale attempt big" << std::endl;
-
-		for (int i = 0; i < 8; i++)
-		{
-			cube[i] = Matrix3::Scale(101, 101) * cube[i];
-
-		}
-		vertex[0].coordinate[0] = (cube.at(0).x); //3
-		vertex[0].coordinate[1] = (cube.at(0).y); //3
-		vertex[0].coordinate[2] = (cube.at(0).z); //3
-
-		vertex[1].coordinate[0] = (cube.at(1).x); //1
-		vertex[1].coordinate[1] = (cube.at(1).y); //1
-		vertex[1].coordinate[2] = (cube.at(1).z); //1
-
-		vertex[2].coordinate[0] = (cube.at(2).x); //2
-		vertex[2].coordinate[1] = (cube.at(2).y); //2
-		vertex[2].coordinate[2] = (cube.at(2).z); //2
-
-		vertex[3].coordinate[0] = (cube.at(3).x); //3
-		vertex[3].coordinate[1] = (cube.at(3).y); //3
-		vertex[3].coordinate[2] = (cube.at(3).z); //3
-
-		vertex[4].coordinate[0] = (cube.at(4).x); //1
-		vertex[4].coordinate[1] = (cube.at(4).y); //1
-		vertex[4].coordinate[2] = (cube.at(4).z); //1
-
-		vertex[5].coordinate[0] = (cube.at(5).x); //2
-		vertex[5].coordinate[1] = (cube.at(5).y); //2
-		vertex[5].coordinate[2] = (cube.at(5).z); //2
-
-		vertex[6].coordinate[0] = (cube.at(6).x); //1
-		vertex[6].coordinate[1] = (cube.at(6).y); //1
-		vertex[6].coordinate[2] = (cube.at(6).z); //1
-
-		vertex[7].coordinate[0] = (cube.at(7).x); //2
-		vertex[7].coordinate[1] = (cube.at(7).y); //2
-		vertex[7].coordinate[2] = (cube.at(7).z); //2
-	}
-
 	//cout << "Update up" << endl;
-
 }
 
 void Game::render()
--- opengl-3-electric-bigalee/master/SFMLOpenGL/Vector3.h
+++ opengl-3-electric-bigalee/master/SFMLOpenGL/Vector3.h
@@ -1,43 +1,10 @@
-#pragma once
-
-#include <math.h>
-#include <string>
-#include <iostream>
-
 namespace gpp
 {
 	class Vector3
 	{
 	public:
-		float x;
-		float y;
-		float z;
-
-		Vector3();
-		Vector3(float x1, float y1, float z1);
-
-		float Length();
-
-		float LengthSquared();
-
-		void Normalise();
-
-		Vector3 operator +(Vector3 V2);
-
-		Vector3 operator -(Vector3 V2);
-
-		Vector3 operator -();
-
-		float operator *(Vector3 V2);
-
-		Vector3 operator *(float k);
-
-		Vector3 operator *(int k);
-
-		Vector3 operator ^(Vector3 V2);
-
-		Vector3 Rotate(int _angle);
-
-		std::string ToString();
+		Vector3() {};
+		~Vector3() {};
+	private:
 	};
 }
